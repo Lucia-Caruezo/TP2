b45fe133e2bfd609651db62a22abf0fd4e9bf319
Lucia-Caruezo <luciacaruezo@gmail.com>
2024-06-26 10:12:50 -0300

    Update Admin.sol

--- desarrollo-de-una-dao-main/contracts/Admin.sol
+++ desarrollo-de-una-dao-main/contracts/Admin.sol
@@ -6,34 +6,38 @@ import "/Member.sol";
 contract Admin is Member {
     mapping(address => bool) public admins;
 
-    constructor(address _daoAddress) Member(_daoAddress) {
+    // CHEQUEAR Agregamos al creador como administrador
+    constructor() Member() {
         admins[msg.sender];
     }
 
+    // Eventos
     event AdminAdded(address admin);
     event AdminRemoved(address admin);
 
+    // Funcion para pasar a ser administrador
     function addAdmin(address _admin) external payable onlyMember {
         require(!admins[_admin], "El administrador ya esta registrado");
-        /// modificar para que 
+        // El admin debe llevar por lo menos 5 dias como miembro
         require(joinDate[_admin]>= block.timestamp - 5 days, "El miembro no cumple con la duracion requerida para ser administrador");
         require(msg.value >= 2 ether, "Debe depositar al menos 2 ETH");
 
-        //Transferir los fondos a la DAO
-        payable(daoAddress).transfer(msg.value);
+        payable(daoAddress).transfer(msg.value); //Transferir los fondos a la DAO
 
         admins[_admin] = true;
-        members[_admin] = true;  // Los administradores también son miembros
         emit AdminAdded(_admin);
     }
 
+    //Funcion para abandonar la DAO
     function removeAdmin(address _admin) external onlyMember {
         require(admins[_admin], "El administrador no existe");
+        require(msg.sender==_admin, "Solo el admin puede elegir abandonar la DAO"); //Para evitar eliminaciones arbitrarias
         admins[_admin] = false;
         members[_admin] = false;
         emit AdminRemoved(_admin);
     }
 
+    // Función para verificar si una dirección es administrador
     function isAdmin(address _address) public view returns (bool) {
         return admins[_address];
     }
